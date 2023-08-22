7a532957ca61d961a4e2b47e2bc00c9335687b25
Camila Lamarque <camilalamarque10@gmail.com>
2023-08-22 08:26:58 -0300

    deucerto

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const app = express();
 // Set up mongoose connection
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
-const mongoDB = "mongodb+srv://arthurjorge:arthur030406@cluster0.u8cdkki.mongodb.net/?retryWrites=true&w=majority";
+const mongoDB = "mongodb+srv://arthur:@rthur030406@cluster0.mc1ygoe.mongodb.net/?retryWrites=true&w=majority";
 
 main().catch((err) => console.log(err));
 async function main() {
